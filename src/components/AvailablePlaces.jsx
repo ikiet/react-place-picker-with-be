25dd2bdd73c1ac5d6c6196e2bc94752aaf772f7a
Kiet Cao <kietcao@tuta.io>
2024-07-21 22:11:19 +0900

    add fetch available places logic

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,22 @@
 import Places from './Places.jsx';
+import { useFetch } from '../hooks/useFetch.js';
+import { getPlaces } from '../http.js';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const {
+    fetchedData: availablePlaces,
+    isFetching,
+    error,
+  } = useFetch(getPlaces, []);
+  if (error) {
+    return (<Error title="An error occurred!" message={error.message} />);
+  }
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      loading={isFetching}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
